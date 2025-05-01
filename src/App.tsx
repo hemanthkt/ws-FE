29cc1d16ed014e824bc73e81cae3a0acb5cc414f
hemanthkt <hmkt1503@gmail.com>
2025-05-01 16:10:45 +0530

    ping pong client side

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>hi</>;
+  const [socket, setSocket] = useState();
+  const messageref = useRef();
+  function sendMessage() {
+    if (!socket) {
+      return;
+    }
+    const message = messageref.current.value;
+    //@ts-ignore
+    socket.send(message);
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
+
+    ws.onmessage = (e) => {
+      alert(e.data);
+    };
+  }, []);
+
+  return (
+    <div>
+      <input type="text" placeholder="MEssage..." ref={messageref} />
+      <button onClick={sendMessage}></button>
+    </div>
+  );
 }
 
 export default App;
